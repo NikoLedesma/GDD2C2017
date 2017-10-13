2b8008a652529d0b74a8752c6a8b458da65c8adf
Diego <diego.guida12@gmail.com>
2017-10-12 21:29:51 -0300

    subo abm de sucursal. Permite realizar el alta de una suc. Falta validacion cod postal

--- project/PagoAgilFrba/AbmSucursal/BajaSucursalForm.cs
+++ project/PagoAgilFrba/AbmSucursal/BajaSucursalForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PagoAgilFrba.AbmSucursal
 {
-    public partial class Form1 : Form
+    public partial class BajaSucursalForm : Form
     {
-        public Form1()
+        public BajaSucursalForm()
         {
             InitializeComponent();
         }
---
+++ project/PagoAgilFrba/AbmSucursal/altaSucursalForm.cs
@@ -0,0 +1,50 @@
+﻿using Business;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoAgilFrba.AbmSucursal
+{
+    public partial class altaSucursalForm : Form
+    {
+
+        private Form prevForm;
+       // private BusinessClienteImpl businessClienteImpl;
+        private BusinessSucursalImpl businessSucursalImpl;
+        public altaSucursalForm(Form form)
+        //public altaSucursalForm()
+        {
+            InitializeComponent();
+            businessSucursalImpl = new BusinessSucursalImpl();
+            prevForm = form;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SucursalDTO sucursalDTO = new SucursalDTO();
+            sucursalDTO.nombre = txtNombre.Text;
+            sucursalDTO.direccion = txtDireccion.Text;
+         /* 
+          * COMENTO TODO ESTO PORQUE EN EL DER ESTA EL CAMPO DIRECCION SOLO
+            sucursalDTO.nroPiso = Int32.Parse(txtNumPiso.Text);
+            sucursalDTO.departamento = txtDepartamento.Text.ToCharArray()[0];
+            sucursalDTO.localidad = txtLocalidad.Text;
+         */
+            sucursalDTO.codPostal = txtCodPostal.Text;
+            businessSucursalImpl.saveSucursal(sucursalDTO);
+
+            MessageBox.Show("Se dio de alta la SUCURSAL");
+
+        }
+
+    }
+
+}
