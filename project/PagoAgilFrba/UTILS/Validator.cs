50d731ef11467ace0c23925911114fe01ccfce9e
NikoLedesma <nicolase.ledesma@gmail.com>
2017-11-04 19:08:07 -0300

    verificacion del mail

--- project/PagoAgilFrba/UTILS/Validator.cs
+++ project/PagoAgilFrba/UTILS/Validator.cs
@@ -13,7 +13,7 @@ namespace PagoAgilFrba.UTILS
 
         private readonly static String MODIFICAR_COLUMN = "ModificarColumn";
         private readonly static String BAJAR_COLUMN = "BajarColumn";
-        private readonly static String ELIMINAR_COLUMN = "EliminarColumns";
+        private readonly static String ELIMINAR_COLUMN = "EliminarColumn";
         private readonly static String MSG_INTEGERP_VALIDATION = "EL CAMPO {0} TIENE QUE SER ENTERO POSITIVO";
         private readonly static String MSG_MAIL_VALIDATION = "EL MAIL {0} ES INVALIDO";
         private readonly static String MSG_OBLIGATORY_VALIDATION = "EL CAMPO {0} ES OBLIGATORIO";
